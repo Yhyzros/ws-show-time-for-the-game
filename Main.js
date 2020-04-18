f1be6616abcdf2eae6a7e60a81b53170c362003d
三海伊月 <63846667+Yhyzros@users.noreply.github.com>
2020-04-18 01:22:08 +0000

    JS

--- Main.js
+++ Main.js
@@ -3,8 +3,12 @@ const crypto = require('crypto');
 const fs = require('fs');
 const uuid = () =>([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>(c ^ (crypto.randomBytes(1)[0] & (15 >> (c / 4)))).toString(16));
 const ip = '127.0.0.1';
+
+//Date显时间并截取下来----
 let D = Date();
 let time = D.substr(16,8);
+//有三个点，每个点有两个数字，位置用substr截取为16,8.
+
 const wss = new WebSocket.Server({ port: 17439 });
 console.log('['+ time +']Server is running at '+ ip + ':17439.');
 console.log('Author by Yhyzros.');
